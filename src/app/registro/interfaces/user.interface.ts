e8dbb63aef705b74a1f3506962fd3350e1e59c47
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-10-26 18:19:28 -0400

    Datos del formulario obtenidos

---
+++ src/app/registro/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface User{
+  name: string;
+  userName: string;
+  password: string;
+  password2: string;
+  userRoll: string;
+}
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './interfaces/user.interface';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  user: User = {
+    name: '',
+    userName: '',
+    password: '',
+    password2: '',
+    userRoll: ''
+  };
+
+  //ESTUDIANTE , DOCENTE
+
+  estudiante:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  submitEstudiante():void{
+    this.validar();
+    console.log('estudiante');
+  }
+
+  submitDocente():void{
+    this.validar();
+    console.log('docente');
+  }
+
+  cambiarEstudiante():void{
+    this.estudiante = true;
+  }
+
+  cambiarDocente():void{
+    this.estudiante = false;
+  }
+
+  validar():boolean{
+    return true;
+  }
 }
