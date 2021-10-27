1ad7cd0adb9777a34e5f5fbfd4b8f775ea7271ba
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-10-27 16:30:41 -0400

    Validaciones de campos realizadas

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -45,7 +45,23 @@ export class RegistroComponent implements OnInit {
 
   validar():boolean{
     if(!this.user.name.match('^[a-zA-Z\\s]{3,50}$')){
+
       alert('El nombre debe contener entre 3 y 50 caracteres, y solo se permite el uso del espacio como caracter especial')
+
+    }else if (!this.user.userName.match('^[a-zA-Z][a-zA-Z0-9]{4,}$')) {
+
+      alert('El nombre de usuario debe tener minimo 5 caracteres, y no se permite el uso de caracteres especiales, ademas el nombre no debe empezar con un numero')
+
+    } else if (!this.user.password.match('^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,}$')) {
+
+      alert('Password is not valid: La contraseña debe contener 8 caracteres como minimo, al menos una mayuscula y un numero, ademas de no contener espacios')
+
+    } else if (this.user.password !== this.user.password2) {
+
+      alert('Ambas contraseñas deben ser iguales')
+
+    } else {
+
     }
     return true;
   }
