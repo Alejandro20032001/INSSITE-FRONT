70a1c2ec7e118c1037845d489421abb4f1bc5b89
Alejandro20032001 <dilanantezama@gmail.com>
2021-10-28 20:34:20 -0400

    cambio de nombre

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/inssite1'));
+app.use(express.static(__dirname + '/dist/inssite'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/inssite1/index.html'));});
+'/dist/inssite/index.html'));});
 app.listen(process.env.PORT || 8080);
 
