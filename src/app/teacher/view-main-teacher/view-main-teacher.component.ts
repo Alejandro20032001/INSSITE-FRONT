e694bb4456bcd093c38549d0ac2364382dfa3401
achigy <gonzalesalison775@gmail.com>
2021-11-03 17:50:06 -0400

    Correcion nombres para seguir estandares

--- src/app/teacher/view-main-teacher/view-main-teacher.component.ts
+++ src/app/teacher/view-main-teacher/view-main-teacher.component.ts
@@ -11,15 +11,11 @@ import { CourseService } from 'src/app/courses/services/course.service';
 })
 export class ViewMainTeacherComponent implements OnInit {
   showFiller = false;
-
   centered = false;
   disabled = false;
   unbounded = false;
-  //radius: number;
-  //color: string;
   goHome(){}
   goAddCourse(){}
-
 
   courses!: Course[];
   constructor(private courseService: CourseService, private router: Router) { }
