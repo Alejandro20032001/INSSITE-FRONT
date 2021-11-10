723ec68abd446430d9d16dafa8ebaa42f0749205
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-11-09 22:02:40 -0400

    Anadida ventana principal

--- src/app/anadirMaterialModulo/ventana-principal-anadir-material-modulo/ventana-principal-anadir-material-modulo.component.ts
+++ src/app/anadirMaterialModulo/ventana-principal-anadir-material-modulo/ventana-principal-anadir-material-modulo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Lectura } from './entities/lectura';
+import { LinkMaterial } from './entities/LinkMaterial';
 import { Material } from './entities/material';
+import { Tarea } from './entities/Tarea';
+import { Videollamada } from './entities/Videollamada';
 
 @Component({
   selector: 'app-ventana-principal-anadir-material-modulo',
@@ -13,6 +17,16 @@ export class VentanaPrincipalAnadirMaterialModuloComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    let lectura = new Lectura("Algo","algo");
+    let link = new LinkMaterial("algo","algo");
+    let video = new Videollamada("algo",new Date(),"algo");
+    let tarea = new Tarea(new Date(),1,"");
+    this.materiales[0] = lectura;
+    this.materiales[1] = link;
+    this.materiales[2] = video;
+    this.materiales[3] = tarea;
+
   }
 
 
