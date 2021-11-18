49bfb389ffd519a435f69c25a019669e7bccde3b
achigy <gonzalesalison775@gmail.com>
2021-11-18 10:16:10 -0400

    Pruebas del put

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -11,50 +11,48 @@ export class FilterPipe implements PipeTransform {
     const resultPosts = [];
     console.log(date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear());
     for (const course of value) {
-     console.log(course.dateStartEnroles);
-    var xMonth=course.dateStartEnroles.substring(5, 7);  
-    var xYear=course.dateStartEnroles.substring(0, 4);  
-    var xDate=course.dateStartEnroles.substring(8,10);
-    console.log(xMonth+"  "+xYear+" "+xDate);
-  if (xYear> date.getFullYear())  
-    {  
-      if (course.courseName.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
-        resultPosts.push(course);
-      };
-      if (course.areaCourse.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
-        resultPosts.push(course);
-      };  
-    }  
-    else  
-    {  
-      if (xYear == date.getFullYear())  
-      {   
-        if (xMonth> date.getMonth()+1)  
-        {  
-          if (course.courseName.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
-            resultPosts.push(course);
-          };
-          if (course.areaCourse.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
-            resultPosts.push(course);
-          };  
-        }  
-        else  
+      
+      let dateEnrole: Date =new Date(course.dateStartEnrole);
+       console.log(course.dateStartEnrole);
+    if (dateEnrole.getFullYear()> date.getFullYear())  
+      {  
+        if (course.courseName.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
+          resultPosts.push(course);
+        };
+        if (course.areaCourse.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
+          resultPosts.push(course);
+        };  
+      }  
+      else  
+      {  
+        if (dateEnrole.getFullYear() == date.getFullYear())  
         {   
-          if (xMonth == date.getMonth()+1)  
+          if (dateEnrole.getMonth()> date.getMonth()+1)  
           {  
-            if (xDate> date.getDate())  
             if (course.courseName.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
               resultPosts.push(course);
             };
             if (course.areaCourse.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
               resultPosts.push(course);
             };  
-           };  
-          }; 
+          }  
+          else  
+          {   
+            if (dateEnrole.getMonth()+1 == date.getMonth()+1)  
+            {  
+              if (dateEnrole.getDate()> date.getDate())  
+              if (course.courseName.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
+                resultPosts.push(course);
+              };
+              if (course.areaCourse.toLowerCase().indexOf(arg.toLowerCase()) > -1 ) {
+                resultPosts.push(course);
+              };  
+             };  
+            }; 
+          };
         };
+  
       };
-
-    };
     if(resultPosts.length!=0){
        return resultPosts;
     }else{
--- src/app/student/services/courses.service.ts
+++ src/app/student/services/courses.service.ts
@@ -16,8 +16,17 @@ export class CoursesService {
    //return this.http.get<Course[]>(this.apiURL+'/products');//pruebas
    return this.http.get<Course[]>(this.apiURL+'/user/courses');
   }
-  createCourse(course:Course):Observable<Course>{
+  createCourse(course:Course){
    // return this.http.put<Course>(this.apiURL+'/products', course); //pruebas
-   return this.http.put<Course>(this.apiURL +'/'+ course.idCourse, course);
+   
+   console.log("LLego service");
+   let con = this.http.put<any>('https://inssite-database.herokuapp.com/course/672d7d24-0194-4569-b354-75b4e94950c0',{}).subscribe({
+    error: error => {
+        console.error('There was an error!', error);
+    }
+});
+   console.log(con);
+   return con;
+   //return this.http.put("inssite-database/course/id",course);
   }
 }
