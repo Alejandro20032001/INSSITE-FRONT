3f02d953ec97d9997376d3eb57d6d958f090a4bb
achigy <gonzalesalison775@gmail.com>
2021-11-21 11:26:08 -0400

    Cambios para hacer pruebas

--- src/app/student/services/courses.service.ts
+++ src/app/student/services/courses.service.ts
@@ -13,20 +13,20 @@ export class CoursesService {
   constructor(private http: HttpClient) { }
 
   getProducts():Observable<Course[]>{
-   //return this.http.get<Course[]>(this.apiURL+'/products');//pruebas
+   //return this.http.get<Course[]>('http://localhost:3000'+'/products');//pruebas
    return this.http.get<Course[]>(this.apiURL+'/user/courses');
   }
   createCourse(course:Course){
-   // return this.http.put<Course>(this.apiURL+'/products', course); //pruebas
-   
+  // return this.http.put<Course>('http://localhost:3000'+'/products', course); //pruebas
+  // /*
    console.log("LLego service");
    let con = this.http.put<any>('https://inssite-database.herokuapp.com/course/672d7d24-0194-4569-b354-75b4e94950c0',{}).subscribe({
     error: error => {
         console.error('There was an error!', error);
     }
-});
+   });
    console.log(con);
    return con;
-   //return this.http.put("inssite-database/course/id",course);
+   //*/
   }
 }
