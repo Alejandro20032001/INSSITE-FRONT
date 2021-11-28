fab951ad17e5d5333db53ce85c74b0d3b50640b7
achigy <gonzalesalison775@gmail.com>
2021-11-28 09:01:33 -0400

    Ruta de los botones de la barra lateral

--- src/app/module/contenido/contenido.component.ts
+++ src/app/module/contenido/contenido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contenido',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContenidoComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
+  }
+   verTemas(){
+
+   }
+   subirTareas(){
+
+   }
+  
+   
+   logOut(): void {
+    this.router.navigate(['./login']);
+  }
+  miscursos(): void {
+    this.router.navigate(['./studentCourses']);
+  }
+
+  goHome(): void {
+    this.router.navigate(['./studentWelcomeView']);
+  }
+
+  goTask(){
+    
   }
 
 }
