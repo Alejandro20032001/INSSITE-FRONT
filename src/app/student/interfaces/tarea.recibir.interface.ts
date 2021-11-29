d5f174d57c9e7247fdcd10ef7c6c27d8bb8acb10
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-11-29 16:59:04 -0400

    Corregido la interfaz de la tarea

--- src/app/student/interfaces/tarea.recibir.interface.ts
+++ src/app/student/interfaces/tarea.recibir.interface.ts
@@ -1,5 +1,13 @@
-export interface tareaRecibida{
-  titulo:string,
-  descripcion:string,
-  nota:number
+import { ResourceEnum } from "src/app/anadirMaterialModulo/ventana-principal-anadir-material-modulo/entities/ResourceEnum";
+
+export interface Tarea{
+  "resourceType":ResourceEnum,
+  "title": string,
+  "descriptionResource":string,
+  "content":string,
+  "module":string,
+  "date":Date,
+  "score":Number
+  "idResource":string
+  //coursemodule/resources/idModule get
 }
--- src/app/student/subir-tarea-estudiante/subir-tarea-estudiante.component.ts
+++ src/app/student/subir-tarea-estudiante/subir-tarea-estudiante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { tareaRecibida } from '../interfaces/tarea.recibir.interface';
+import { Tarea } from '../interfaces/tarea.recibir.interface';
+import { ResourceEnum } from "src/app/anadirMaterialModulo/ventana-principal-anadir-material-modulo/entities/ResourceEnum";
 
 @Component({
   selector: 'app-subir-tarea-estudiante',
@@ -8,15 +9,28 @@ import { tareaRecibida } from '../interfaces/tarea.recibir.interface';
 })
 export class SubirTareaEstudianteComponent implements OnInit {
 
-  tarea:tareaRecibida = {
-    titulo: 'Este es el titulo de la tarea',
-    descripcion: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
-    nota: 5
+  tarea:Tarea = {
+    resourceType: ResourceEnum.HOMEWORK,
+    title: 'Este es el titulo de la tarea',
+    descriptionResource: 'Lorem ipsum',
+    content: '',
+    module: '',
+    date: new(Date),
+    score: 5,
+    idResource: ''
   }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  regresar():void{
+
+  }
+
+  enviar():void{
+
   }
 
 }
