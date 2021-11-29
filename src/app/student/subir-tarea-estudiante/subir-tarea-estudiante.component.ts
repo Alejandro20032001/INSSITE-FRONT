b2662d2e36e2f93f34e8557a2315502a22241b82
Alejandro20032001 <dilanantezama@gmail.com>
2021-11-29 18:28:18 -0400

    obtener tareas

--- src/app/student/subir-tarea-estudiante/subir-tarea-estudiante.component.ts
+++ src/app/student/subir-tarea-estudiante/subir-tarea-estudiante.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Tarea } from '../interfaces/tarea.recibir.interface';
 import { ResourceEnum } from "src/app/anadirMaterialModulo/ventana-principal-anadir-material-modulo/entities/ResourceEnum";
+import { ResourcesService } from '../services/resources.service';
 
 @Component({
   selector: 'app-subir-tarea-estudiante',
   templateUrl: './subir-tarea-estudiante.component.html',
   styleUrls: ['./subir-tarea-estudiante.component.scss']
 })
+
 export class SubirTareaEstudianteComponent implements OnInit {
 
   tarea:Tarea = {
@@ -20,9 +22,13 @@ export class SubirTareaEstudianteComponent implements OnInit {
     idResource: ''
   }
 
-  constructor() { }
+  constructor(private resourceService: ResourcesService) { }
 
   ngOnInit(): void {
+    let idTarea = "b68e82e3-d726-4505-966b-e778bb730265";// falta implementar
+    this.resourceService.getResourceId(idTarea).subscribe((data) =>
+      this.tarea = data  
+    )
   }
 
   regresar():void{
