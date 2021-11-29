7a07095d8fe03b4c7537dde77445acea75b9745d
achigy <gonzalesalison775@gmail.com>
2021-11-29 03:36:40 -0400

    Especificar atributos de modulo

---
+++ src/app/models/modulo.inteface.ts
@@ -0,0 +1,10 @@
+import { Course } from "../student/interfaces/course.interface";
+
+export interface Module{
+ 
+  "idModule": string, 
+  "nameModule":string, 
+  "descriptionModule": string,
+   "durationModule": number, 
+   "course": Course
+}
