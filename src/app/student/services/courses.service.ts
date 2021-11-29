4de8c263ddea16a8d44f5f78878bf16f83a51d94
achigy <gonzalesalison775@gmail.com>
2021-11-29 03:39:59 -0400

    Obtencion de los modulos de un curso

--- src/app/student/services/courses.service.ts
+++ src/app/student/services/courses.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../interfaces/course.interface';
+import { CookieService } from 'ngx-cookie-service';
+import { Module } from 'src/app/models/modulo.inteface';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +14,26 @@ export class CoursesService {
 
   private apiURL = 'https://inssite-database.herokuapp.com';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
+
+  listModulo:Module[] = [];
+  
+  ngOnInit(): void {
+    this.cookieService.set('idCourse',"672d7d24-0194-4569-b354-75b4e94950c0");
+  }
+
+  
 
   getProducts():Observable<Course[]>{
    //return this.http.get<Course[]>('http://localhost:3000'+'/products');//pruebas
    return this.http.get<Course[]>(this.apiURL+'/user/courses');
   }
+
+  getModules():Observable<Module[]>{
+    //return this.http.get<Course[]>('http://localhost:3000'+'/products');//pruebas
+    return this.http.get<Module[]>(this.apiURL+'/coursemodule/'+this.cookieService.get('idCourse'));
+   }
+  
   createCourse(course:Course){
   // return this.http.put<Course>('http://localhost:3000'+'/products', course); //pruebas
   // /*
