a41a0187a7dd3d09fdf67256f079dffec777b8e6
achigy <gonzalesalison775@gmail.com>
2021-12-02 21:23:59 -0400

    Mostrar tareas y boton de modulos

--- src/app/module/contenido/contenido.component.ts
+++ src/app/module/contenido/contenido.component.ts
@@ -7,6 +7,7 @@ import {tap} from 'rxjs/operators';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CoursesService } from 'src/app/student/services/courses.service';
 import { Module } from 'src/app/models/modulo.inteface';
+import { ResourceEnum } from 'src/app/anadirMaterialModulo/ventana-principal-anadir-material-modulo/entities/ResourceEnum';
 
 @Component({
   selector: 'app-contenido',
@@ -23,11 +24,11 @@ export class ContenidoComponent implements OnInit {
   campaignTwo: FormGroup;
   aleatorio!: String;
   link!:string;
+  mostrar:boolean=false;
 
 
   Frases:string[]= ["Si en los inicios no puedes alimentar a tu equipo con 2 pizzas, es que es demasiado grande – Jeff Bezos de Amazon.",
   "El valor de una idea radica en el uso de la misma – Thomas Edison.",
-  "El trabajo va a ocupar gran parte de tu vida. La única forma de estar realmente satisfecho es hacer aquello que crees que es un buen trabajo, y la única forma de hacer un gran trabajo es amar aquello que haces – Steve Jobs de Apple.",
    "Tus clientes más insatisfechos deben ser tu mayor fuente de aprendizaje – Bill Gates",
   "Una visión de una idea sin la capacidad de ejecución es únicamente una alucinación – Steve Case de AOL."];
 
@@ -51,27 +52,19 @@ export class ContenidoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.cookieService.set('idModulo',"1ff3ba00-e970-4ee1-abbc-b77411a479ac");
+    this.courseService.getModules().
+    pipe(
+      tap((listModule:Module[]) => this.listModule = listModule.sort( (a, b) => (a.orderModule > b.orderModule) ? 1 : -1))
+    )
+    .subscribe();
+    this.cookieService.set('idModulo',"23fb49d0-c51b-4dc8-9a39-cb8ca66a295e");
     this.servicios.obtenerMaterialModulo(this.cookieService.get('idModulo')).
     pipe(
       tap((materiales:MaterialLista[]) => this.materiales = materiales.reverse())
     )
     .subscribe();
-
-    this.courseService.getModules().
-    pipe(
-      tap((listModule:Module[]) => this.listModule = listModule.sort( (a, b) => (a.order < b.order) ? 1 : -1))
-    )
-    .subscribe();
-    console.log(this.listModule);
-    console.log(this.materiales);
   }
-   verTemas(){
-   }
-   subirTareas(){
-
-   }
-  
+   subirTareas(){}
    
    logOut(): void {
     this.router.navigate(['./login']);
@@ -93,25 +86,21 @@ export class ContenidoComponent implements OnInit {
    this.aleatorio = this.Frases[Math.floor(Math.random() * this.Frases.length)];
    return this.aleatorio;
   }
-  ultimoLink():void{
-    //for(let material of this.materiales){
-      //if (material.resourceType=="LINK A MATERIAL"){
-        
-        this.link='www.google.com'//material.content;
-        var a=document.getElementById('link');
-        if(a!=null){
-          a.setAttribute("href",this.link); 
-          console.log("aqui");
-        }
-      //}
-    //}
+  
+  llenarM(id:string){
+   
   }
-  esTarea(mat:MaterialLista):boolean{
-    for(let material of this.materiales){
-      if (material.resourceType=='TAREA'){
-        return true;
-      }
-    }
-    return false;
-  }
+  
+ mostrarF(){
+   this.mostrar=!this.mostrar;
+ }
+ esTarea(mat:ResourceEnum):boolean{
+     if(mat===ResourceEnum.HOMEWORK){
+       return true;
+     }
+     return false;
+ }
+ volver(){
+
+ }
 }
