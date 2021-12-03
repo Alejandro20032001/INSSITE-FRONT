e5412dab83cf209a31feaab6cb8647430016b267
achigy <gonzalesalison775@gmail.com>
2021-12-02 23:05:59 -0400

    Mostrar frase sino existe material y correciones de diseño

--- src/app/module/contenido/contenido.component.ts
+++ src/app/module/contenido/contenido.component.ts
@@ -27,10 +27,19 @@ export class ContenidoComponent implements OnInit {
   mostrar:boolean=false;
 
 
-  Frases:string[]= ["Si en los inicios no puedes alimentar a tu equipo con 2 pizzas, es que es demasiado grande – Jeff Bezos de Amazon.",
+  Frases:string[]= [
   "El valor de una idea radica en el uso de la misma – Thomas Edison.",
    "Tus clientes más insatisfechos deben ser tu mayor fuente de aprendizaje – Bill Gates",
-  "Una visión de una idea sin la capacidad de ejecución es únicamente una alucinación – Steve Case de AOL."];
+  "Una visión de una idea sin la capacidad de ejecución es únicamente una alucinación – Steve Case de AOL.",
+  "El único modo de hacer un gran trabajo es amar lo que haces - Steve Jobs",
+  "El dinero no es la clave del éxito; la libertad para poder crear lo es - Nelson Mandela",
+  "Cuanto más duramente trabajo, más suerte tengo - Gary Player",
+  "El trabajo duro hace que desaparezcan las arrugas de la mente y el espíritu - Helena Rubinstein",
+  "Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan - Elon Musk",
+  "Escoge un trabajo que te guste, y nunca tendrás que trabajar ni un solo día de tu vida - Confucio",
+  "Un sueño no se hace realidad por arte de magia, necesita sudor, determinación y trabajo duro - Colin Powell",
+  "Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé - Benjamin Franklin", 
+  "La lógica te llevará de la a a la z. la imaginación te llevará a cualquier lugar - Albert Einstein"];
 
 
   constructor(
