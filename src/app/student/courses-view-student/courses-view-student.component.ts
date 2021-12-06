9571dd62506f5b0e82c546bef65f7d8b64b110b5
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-12-05 20:24:13 -0400

    Navegacion entre ventana de cursos y modulos anadida

--- src/app/student/courses-view-student/courses-view-student.component.ts
+++ src/app/student/courses-view-student/courses-view-student.component.ts
@@ -3,6 +3,7 @@ import { CoursesService } from '../services/courses.service';
 import {tap} from 'rxjs/operators';
 import { Course } from '../interfaces/course.interface';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-courses-view-student',
@@ -13,7 +14,7 @@ export class CoursesViewStudentComponent implements OnInit {
 
   courses: Course[] = [];
 
-  constructor(private productSvc: CoursesService, private router:Router) { }
+  constructor(private productSvc: CoursesService, private router:Router, private cookieService: CookieService) { }
 
   ngOnInit(): void {
 
@@ -33,5 +34,10 @@ export class CoursesViewStudentComponent implements OnInit {
   }
   search():void{
     this.router.navigate(['./searchCourse']);
+  }
+
+  irCurso(id: string):void{
+    this.router.navigate(['./modulosVistaEstudiante']);
+    this.cookieService.set('idModulo',id);
   }
 }
