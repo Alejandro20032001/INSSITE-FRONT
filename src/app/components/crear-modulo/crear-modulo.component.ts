1757357972ca90a82ff5ef4af4c9edc1f515b203
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-12-06 21:19:33 -0400

    Correcion en la cantidad de dias

--- src/app/components/crear-modulo/crear-modulo.component.ts
+++ src/app/components/crear-modulo/crear-modulo.component.ts
@@ -20,10 +20,14 @@ export class CrearModuloComponent implements OnInit {
   }
 
   guardarModulo(){
-    console.log("Hola");
     if(this.moduloName.replace(" ","") === ""){
       alert("Debes ingresar un nombre para el modulo");
-    }else{
+    }else if(this.duracion < 7){
+      alert("Un modulo debe durar al menos 7 dias");
+    }else if(this.duracion > 30){
+      alert("Un modulo no puede durar mas de 30 dias");
+    }
+    else{
       this.clickGuardar.emit(new Modulo(this.idModulo, this.moduloName, this.duracion, this.order, 0));
       this.moduloName = '';
       this.duracion = 0;
