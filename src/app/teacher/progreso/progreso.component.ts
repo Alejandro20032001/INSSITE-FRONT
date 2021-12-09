52e68777f63dba5ead1c8e9baa1382c525ac55eb
achigy <gonzalesalison775@gmail.com>
2021-12-09 19:40:16 -0400

    Cambio de diseno pero aun con bug :'(

--- src/app/teacher/progreso/progreso.component.ts
+++ src/app/teacher/progreso/progreso.component.ts
@@ -3,31 +3,24 @@ import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { tap } from 'rxjs/operators';
 import { CourseService } from 'src/app/courses/services/course.service';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 import { userSend } from 'src/app/registro/interfaces/userSend.interface';
 import { RegisterServices } from 'src/app/registro/services/user.service';
-import { MaterialLista } from 'src/app/anadirMaterialModulo/ventana-principal-anadir-material-modulo/interfaces/materialMenu.interface';
 import { Homework } from 'src/app/student/interfaces/homework.interface';
+
+
 
 @Component({
   selector: 'app-progreso',
   templateUrl: './progreso.component.html',
   styleUrls: ['./progreso.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
 })
 export class ProgresoComponent implements OnInit {
   dataSource!:userSend[];
-  columnsTo =['Nombre Completo'] ;
-  columnsToDisplay = ['completeName'] ;
-  displayedColumns=['No','Tarea','Puntuacion']
-  expandedElement!: userSend | null;
-  numTareas:number=3;
+  columns: string[] = ['Nombre Completo'];
+  displayedColumns: string[] = ['completeName'];
+  mostrar:boolean=false;
+
+
   tareas!:any;
   tareasHechas!:Homework[];
   constructor(private router : Router,
@@ -37,13 +30,10 @@ export class ProgresoComponent implements OnInit {
   ngOnInit(): void {
     this.cookieService.set('idCourse','ac16e052-d0cb-45c9-a7c5-c97ad093b298');
     this.servicios.getStudents('ac16e052-d0cb-45c9-a7c5-c97ad093b298').pipe(
-      tap((dataSource:userSend[]) => this.dataSource = dataSource.sort())
+      tap((dataSource:userSend[]) => this.dataSource = dataSource.reverse())
     )
     .subscribe();
-    console.group();
-    console.log(this.dataSource);
-    console.groupEnd();
-  }//this.cookieService.get('idModulo')).
+    }
   
 
   goAddCourse():void{
@@ -53,24 +43,33 @@ export class ProgresoComponent implements OnInit {
   logout():void{
     this.router.navigate(['./login']);
   }
+
+
   goCourses(){
     this.router.navigate(['./teacher']);
   }
+
+
   studentU(idStudent:string){
     let idC:string =this.cookieService.get('idCourse');
     this.student(idC,idStudent);
   }
+
+
   student(idC:string,idU:string){
+    this.mostrar=false;
     this.userservice.getprogreso(idC,idU).pipe(
       tap((tareas:any) => this.tareas = tareas)
     )
     .subscribe();   
-
-    console.log('llega');
+    if(this.tareas.length!=0){
+      this.mostrar=true;
+    }  
   }
+
+
   cantidad(t:Homework[]):number{
     this.tareasHechas=t;
       return t.length;
   }
-
 }
